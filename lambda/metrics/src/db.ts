8b55edbbdb74163ad7b37ccd1ffc157ed74a0d34
Team Board Developer <team-board@local>
2026-09-17 15:58:36 -0500

    feat: prepare metrics lambda for RDS

--- lambda/metrics/src/db.ts
+++ lambda/metrics/src/db.ts
@@ -9,6 +9,10 @@ const pool = new Pool({
   database: process.env.PGDATABASE,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
+  ssl:
+    process.env.PGSSL === "require"
+      ? { rejectUnauthorized: false }
+      : undefined,
 });
 
 export async function getMetrics(): Promise<Metrics> {
